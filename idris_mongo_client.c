8e4614dc3155f21c2dc3dac2d220cd9391c8eec5
Guillaume Yziquel <guillaume.hj.yziquel@gmail.com>
2018-12-25 21:50:32 +0100

    Whitespacing.

--- idris_mongo_client.c
+++ idris_mongo_client.c
@@ -65,11 +65,11 @@ int idris_mongoc_client_command_simple(const CData client_cdata,
 }
 
 int idris_mongoc_client_write_command_with_opts(const CData client_cdata,
-						                                    const char * db_name,
-						                                    const CData command_cdata,
-						                                    const CData options_cdata,
+						const char * db_name,
+						const CData command_cdata,
+						const CData options_cdata,
                                                 const CData reply_cdata,
-						                                    const CData error_cdata)
+						const CData error_cdata)
 {
   mongoc_client_t * client = (mongoc_client_t *) client_cdata->data;
   const bson_t * command = (const bson_t *) command_cdata->data;
