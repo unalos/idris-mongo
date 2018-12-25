5f2370e1914aaa9db4eb789911f74e4a65db4273
Guillaume Yziquel <guillaume.hj.yziquel@gmail.com>
2018-12-25 21:53:17 +0100

    Whitespacing.

--- idris_mongo_options.c
+++ idris_mongo_options.c
@@ -5,7 +5,7 @@ bool idris_mongoc_write_concern_append(const CData write_concern_cdata,
                                        const CData command_cdata)
 {
   mongoc_write_concern_t * write_concern =
-	  (mongoc_write_concern_t *) write_concern_cdata->data;
+    (mongoc_write_concern_t *) write_concern_cdata->data;
   bson_t * command = (bson_t *) command_cdata->data;
   return mongoc_write_concern_append(write_concern, command);
 }
@@ -14,7 +14,7 @@ bool idris_mongoc_read_concern_append(const CData read_concern_cdata,
                                       const CData command_cdata)
 {
   mongoc_read_concern_t * read_concern =
-	  (mongoc_read_concern_t *) read_concern_cdata->data;
+    (mongoc_read_concern_t *) read_concern_cdata->data;
   bson_t * command = (bson_t *) command_cdata->data;
   return mongoc_read_concern_append(read_concern, command);
 }
